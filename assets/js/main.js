2f908412a222e84019cb1e4bd97153c8f9d7fac0
neyamat101 <67000556+neyamat101@users.noreply.github.com>
2020-07-15 12:46:38 +0600

    Update main.js
    client js added

--- assets/js/main.js
+++ assets/js/main.js
@@ -180,6 +180,24 @@
       }
     }
   });
+  
+  // Clients carousel (uses the Owl Carousel library)
+  $(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 6
+      }
+    }
+  });
 
   // Portfolio details carousel
   $(".portfolio-details-carousel").owlCarousel({
